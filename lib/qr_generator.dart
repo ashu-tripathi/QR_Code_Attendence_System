781995af9786fa5e6f106a1ff0d58bea4168e2d0
Shashank <shashank.agrawal_cs18@gla.ac.in>
2022-01-30 18:33:06 +0530

    Qr Generator + Scanner

--- lib/qr_generator.dart
+++ lib/qr_generator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class QrGenerator extends StatefulWidget {
   const QrGenerator({Key key}) : super(key: key);
@@ -8,15 +9,71 @@ class QrGenerator extends StatefulWidget {
 }
 
 class _QrGeneratorState extends State<QrGenerator> {
+  final controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('QR Generator Page'),
-          backgroundColor: Colors.blue,
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('QR Generator Page'),
+            backgroundColor: Colors.teal,
+          ),
+          backgroundColor: Colors.black12,
+          body: Center(
+
+            child: SingleChildScrollView(
+                padding: EdgeInsets.all(24),
+                child: Column(
+
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    QrImage(
+                      data: controller.text,
+                      size: 200,
+                      backgroundColor: Colors.white,
+                      errorStateBuilder: (cxt, err) {
+                        return Container(
+                          child: Center(
+                            child: Text(
+                              "Uh oh! Something went wrong...",
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    SizedBox(height: 40),
+                    buildTextField(context),
+                  ],
+                )),
+          )),
     );
   }
+
+  Widget buildTextField(BuildContext context) => TextField(
+        controller: controller,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 28,
+        ),
+        decoration: InputDecoration(
+          hintText: 'Enter the data',
+          hintStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Theme.of(context).accentColor),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          suffixIcon: IconButton(
+            color: Theme.of(context).accentColor,
+            icon: Icon(Icons.done, size: 30),
+            onPressed: () => setState(() {}),
+          ),
+        ),
+      );
 }
--- lib/qr_scanner.dart
+++ lib/qr_scanner.dart
@@ -1,4 +1,8 @@
+import 'dart:io' show Platform;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class QrScanner extends StatefulWidget {
   const QrScanner({Key key}) : super(key: key);
@@ -8,15 +12,82 @@ class QrScanner extends StatefulWidget {
 }
 
 class _QrScannerState extends State<QrScanner> {
+  final qrkey = GlobalKey(debugLabel: 'QR');
+  QRViewController controller;
+  BuildContext ctx;
+
+  Barcode barcode;
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void reassemble() async {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller.resumeCamera();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('QR Scanner Page'),
+          title: const Text('QR Scanner Page'),
           backgroundColor: Colors.blue,
         ),
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            buildQrView(context),
+            Positioned(bottom: 10, child: buildResult()), // result method
+
+          ],
+        ),
       ),
+    );
+  }
+
+
+  Widget buildResult(){
+
+    return
+    Container(
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.white,
+            ),
+            child: Text(
+              barcode != null
+                  ? 'Your attendance marked Successfully'
+                  : 'Scan a code', // success message
+              maxLines: 3,
+            ),
+          );
+   }
+  Widget buildQrView(BuildContext context) => QRView(
+        key: qrkey,
+        onQRViewCreated: onQrViewCreated,
+        overlay: QrScannerOverlayShape(
+          cutOutSize: MediaQuery.of(context).size.width * 0.8,
+          borderWidth: 10,
+          borderColor: Colors.white,
+          borderRadius: 10,
+          borderLength: 20,
+        ),
+      );
+  void onQrViewCreated(QRViewController controller) {
+    // setState(() => this.controller = controller);
+    this.controller = controller;
+    controller.scannedDataStream.listen(
+      (barcode) => setState(() => this.barcode = barcode),
     );
   }
 }
